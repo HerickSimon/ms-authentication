e99f1068f7cf5a3100e62ee395ab065dc58b0961
Herick Simon Nunes Bandeira <HerickSimon@MacBook-Pro-de-Central.local>
2021-11-28 19:25:49 -0400

    Adição da expiração do token

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,6 @@
 import { Pool } from "pg";
 
-const connectionString =
-  "postgres://ifrdjvxu:4uDi_mOBY5-9z9nuTcQvv_y7J8NsIh4Z@motty.db.elephantsql.com/ifrdjvxu";
+const connectionString = "";
 
 const db = new Pool({connectionString});
 
